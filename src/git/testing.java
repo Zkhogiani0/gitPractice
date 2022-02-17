68ec064c488d2e1af487515c5ce21d9b876efde4
zkhog <Zkhogiani0@gmail.com>
2022-02-17 03:24:26 -0500

    first commit

--- src/git/testing.java
+++ src/git/testing.java
@@ -5,10 +5,7 @@ public class testing {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("water is blue");
-		
-		// feature
 		System.out.println("Hello world");
-		//asfdgfdgfdhg
 		System.out.println("Hello world");
 	}
 
