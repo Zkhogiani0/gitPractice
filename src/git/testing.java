185bc79e93f2563ee96b80bda7dd53ac366e5da9
zkhog <Zkhogiani0@gmail.com>
2022-02-16 20:05:44 -0500

    3rd commit

--- src/git/testing.java
+++ src/git/testing.java
@@ -5,6 +5,9 @@ public class testing {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("water is blue");
+		
+		//
+		System.out.println("Hello world);
 	}
 
 }
